8886829b01c63ada5130480bcae09ccfc163380c
Raul Garcia <raul.garcia@thenesor.com>
2022-05-04 13:38:55 +0100

    Initialise Spring Boot project using Spring initializr
      -  Source from https://start.spring.io/
    
      - Created with Gradle, Java, Packaging as Jar, Spring Boot version 2.6.7
        and Spring Web as dependency.

---
+++ src/main/java/com/hospitality/menu/hospitalitymenuservice/HospitalityMenuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospitality.menu.hospitalitymenuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalityMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalityMenuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitality/menu/hospitalitymenuservice/HospitalityMenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitality.menu.hospitalitymenuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityMenuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

10930fe04cb0d43311fb805bcd478450afae174a
Raul Garcia <raul.garcia@thenesor.com>
2022-07-24 22:07:40 +0100

    Adds Test Coverage enforcement with Jacoco at 100% line coverage

---
+++ service/src/test/java/com/hospitality/menu/ApplicationIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.hospitality.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationIntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- service/src/test/java/com/hospitality/menu/ApplicationTest.java
+++ service/src/test/java/com/hospitality/menu/ApplicationTest.java
@@ -1,13 +1,32 @@
 package com.hospitality.menu;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class ApplicationTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-	@Test
-	void contextLoads() {
-	}
+public class ApplicationTest {
+    @Test
+    public void shouldBeAnnotatedWithExpectedSpringBootApplicationAnnotation() {
+        assertThat(Application.class).hasAnnotation(SpringBootApplication.class);
+    }
 
+    @Test
+    public void main_shouldCallRunSpringApplicationWithAnEmptyListOfArguments() {
+        try (MockedStatic<SpringApplication> springApplication = Mockito.mockStatic(SpringApplication.class)) {
+            Application.main(new String[]{});
+            springApplication.verify(() -> SpringApplication.run(Application.class, new String[]{}));
+        }
+    }
+
+    @Test
+    public void main_shouldCallRunSpringApplicationWithPassedArguments() {
+        try (MockedStatic<SpringApplication> springApplication = Mockito.mockStatic(SpringApplication.class)) {
+            Application.main(new String[]{"arg1", "arg2"});
+            springApplication.verify(() -> SpringApplication.run(Application.class, new String[]{"arg1", "arg2"}));
+        }
+    }
 }
